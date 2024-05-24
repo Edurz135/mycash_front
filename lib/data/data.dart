9fe02aaa4634d16b6969f9bbf43105df7ae7906d
SEBASTIAN GUZMAN SOTO <20200981@aloe.ulima.edu.pe>
2024-05-24 01:24:22 -0500

    Pantalla de estadisticas: Finalización y retoques

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -3,6 +3,21 @@ import 'package:mycash_front/model/datetime_data.dart';
 import 'package:flutter/material.dart';
 import 'package:mycash_front/model/objetive_category_data.dart';
 class Data{
+  static List<Map<String, String>> Ingresos =[
+    {'Titulo': 'Compras', 'Monto':'S/'+'1.400'},
+    {'Titulo': 'Banco', 'Monto':'S/'+'1.400'},
+    {'Titulo': 'Educacion', 'Monto':'S/'+'1.400'},
+    {'Titulo': 'Guardados', 'Monto':'S/'+'1.400'},
+    {'Titulo': 'Academia', 'Monto':'S/'+'5.400'},
+  ];
+
+  static List<Map<String, String>> Egresos = [
+    {'Titulo': 'Renta', 'Monto': 'S/'+'900'},
+    {'Titulo': 'Servicios', 'Monto': 'S/'+'300'},
+    {'Titulo': 'Transporte', 'Monto': 'S/'+'200'},
+    {'Titulo': 'Comida', 'Monto': 'S/'+'400'},
+  ];
+
   static List<ChartData> resultados = [
     ChartData("ingresos", 3000, Colors.blue),
     ChartData("egresos", 2500, Colors.blue),
--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mycash_front/components/stacked_line_sync_fusion_content.dart';
+import 'package:mycash_front/data/data.dart';
 
 
 
@@ -10,6 +11,7 @@ class StatisticsScreen extends StatefulWidget {
 
 class _StatisticsScreenState extends State<StatisticsScreen>{
   String? _dropdownValue;
+  String selectedCategory = 'Ingresos';
   @override
   Widget build(BuildContext context){
     void dropdownCallBack(String? selectedValue){
@@ -22,7 +24,7 @@ class _StatisticsScreenState extends State<StatisticsScreen>{
 
 
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
          ClipRRect(
           borderRadius:
@@ -62,10 +64,207 @@ class _StatisticsScreenState extends State<StatisticsScreen>{
             DropdownMenuItem(child: Text("Anual"), value: "Anual"),
           ], value: _dropdownValue,onChanged: dropdownCallBack),
         StackedLineSyncFusionContent().build(context),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildCategoryButton('Ingresos'),
+              _buildCategoryButton('Egresos'),
+            ],
+          ),
+        ),
+
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: _buildCategoryBlocks(),
+            
+          ),
+        ),
         
+
       ],
     );
   }
+
+  Widget _buildCategoryButton(String category){
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedCategory = category;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        margin: EdgeInsets.symmetric(horizontal: 4.0),
+        decoration: BoxDecoration(
+          color: selectedCategory == category ? Color.fromARGB(255, 187, 96, 203) : Colors.grey,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Text(
+          category,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+      ),
+    );
+  }
+/*
+  List <Widget> _buildCategoryBlocks(){
+    List<Map<String, String>> data = selectedCategory == 'Ingresos'
+      ? Data.Ingresos
+      : Data.Egresos;
+    
+    if (data.isEmpty){
+      return [
+        Center(
+          child: Text(
+            'no data avaliable',
+            style: TextStyle(fontSize: 18.0, color: Colors.red),
+          ),
+        )
+      ];
+    }
+
+    return data.map((item){
+      return Row(children: [
+        Container(
+          width: 150,
+          height: 90,
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 4.0,
+              offset: Offset(0,2),
+            )
+          ],
+          border: Border.all(
+            color: Colors.blue,
+            width: 2.0,
+          )
+        ),
+        
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              item['Titulo'] ?? 'N/A',
+              style:TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 4.0),
+            Text(
+              item['Monto'] ?? 'N/A',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+        
+      )]);
+    }).toList();
+      
+  }
+  */
+  List <Widget> _buildCategoryBlocks(){
+    List<Map<String, String>> data = selectedCategory == 'Ingresos'
+      ? Data.Ingresos
+      : Data.Egresos;
+    
+    if (data.isEmpty){
+      return [
+        Center(
+          child: Text(
+            'no data avaliable',
+            style: TextStyle(fontSize: 18.0, color: Colors.red),
+          ),
+        )
+      ];
+    }
+
+    List<Widget> rows = [];
+  for (int i = 0; i < data.length; i += 2) {
+    if (i + 1 < data.length) {
+      // Agrega una fila con dos contenedores
+      rows.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _buildCategoryBlock(data[i]),
+            _buildCategoryBlock(data[i + 1]),
+          ],
+        ),
+      );
+    } else {
+      // Si quedó un elemento suelto, agrégalo en una fila
+      rows.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildCategoryBlock(data[i]),
+          ],
+        ),
+      );
+    }
+    
+  }
+  return rows;
+  }
+    Widget _buildCategoryBlock(Map<String, String> item) {
+    return Container(
+      width: 150,
+      height: 120,
+      padding: EdgeInsets.all(16.0),
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.deepPurple,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 4.0,
+            offset: Offset(0, 2),
+          )
+        ],
+        border: Border.all(
+          color: Colors.blue,
+          width: 2.0,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            item['Titulo'] ?? 'N/A',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 4.0),
+          Text(
+            item['Monto'] ?? 'N/A',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
-
-
